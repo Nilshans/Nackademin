63305af691fd997cc319892f98679875336ec60d
Nilshans <martineriksson89@gmail.com>
2020-09-03 09:14:52 +0200

    content

--- uppgiftB.py
+++ uppgiftB.py
@@ -1,4 +1,16 @@
 import sys
 
-name = input ("Type name: ")
-print(name)
+names = ["Dan", "Eric", "Martin", "Gustaf", "Anders"]
+ages = [21, 42, 34, 35, 22]
+sizes = [39, 43, 41, 44, 36]
+profileIndex = []
+
+x = 1
+sizes.sort()
+lengthOfSizes = len(sizes)
+medianIndex = (lengthOfSizes+1)/2
+oldestIndex = len(ages)
+
+
+print(medianIndex)
+# print(names[x], ages[x], sizes[x])
